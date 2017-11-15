440c1ec8c6a62b738edd54c7df1e8e45825786bc
Allan Grønlund <jallan@cs.au.dk>
2017-11-15 16:35:16 +0100

    progress bar dawa download

--- cvrparser/cvr_makedb.py
+++ cvrparser/cvr_makedb.py
@@ -39,8 +39,9 @@ class MakeCvrDatabase(object):
         filename = os.path.join(config['data_path'], 'dawa.csv')
         url = 'https://dawa.aws.dk/adresser?format=csv'
         r = requests.get(url, stream=True)
+        total_length = int(r.headers.get('content-length', 0))
         with open(filename, 'wb') as f:
-            total_length = int(r.headers.get('content-length'))
+
             for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length / 1024) + 1):
                 if chunk:
                     f.write(chunk)
