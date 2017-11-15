dd376759c1b7f509ced44ce0c9db485fe7f43929
Allan Grønlund <jallan@cs.au.dk>
2017-11-15 17:06:26 +0100

    progress bar dawa download

--- cvrparser/cvr_makedb.py
+++ cvrparser/cvr_makedb.py
@@ -45,7 +45,8 @@ class MakeCvrDatabase(object):
             #     if chunk:
             #         f.write(chunk)
             #         f.flush()
-            for data in tqdm(r.iter_content(chunk_size=chunk_size), total=total_length/chunk_size, unit_scale=chunk_size):
+            for data in tqdm(r.iter_content(chunk_size=chunk_size), total=total_length/chunk_size,
+                             unit_scale=1.0/chunk_size), unit='MB':
                 f.write(data)
         return filename
         # os.system('wget  https://dawa.aws.dk/adresser?format=csv -O {0}'.format(target))
